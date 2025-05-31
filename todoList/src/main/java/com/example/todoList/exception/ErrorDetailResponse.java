c4f3adb08b203d97b656427f94f7b78d40bafc4f
mandou123 <mingdeee01@gmail.com>
2025-05-31 16:03:25 +0900

    refactor(exception): rename ValidationErrorResponse to ErrorDetailResponse for reuse

--- todoList/src/main/java/com/example/todoList/exception/ErrorDetailResponse.java
+++ todoList/src/main/java/com/example/todoList/exception/ErrorDetailResponse.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class ValidationErrorResponse {
+public class ErrorDetailResponse {
 
     private String field;
     private String message;
